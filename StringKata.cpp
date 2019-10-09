55561a20d1a6f95b8988acdf85ec0c9250ddd244
DaveHalasz <dave.halasz@7signal.com>
2019-10-09 08:36:46 -0400

    Step 1 of Kana

--- StringKata.cpp
+++ StringKata.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
+#include <sstream>
+#include <string>
 using namespace std ;
+
+
+int Add(string numbers);
+
+// Add the numbers
+int Add(string numbers)
+{
+	int Total = 0;
+	
+	stringstream ss;
+	ss << numbers;
+	
+	// Continue until done with the input string
+	// Using a comma as the delimeter
+	string temp;
+	int found;
+	while (getline(ss, temp, ','))
+	{	
+		// Check for an integer
+		if (stringstream(temp) >> found )
+			Total += found;
+	}
+	
+	return Total;
+}
 
 
 int main( int argc, char *argv[] )
 {
-  cout << "StringKata" << endl;
+	string TestStr = "1,2";
+	string NullStr = "";
+	string Test0 = "123,321,111";
+
+  cout << "NullStr ";
+  cout << Add(NullStr) << endl;
+  cout << "TestStr ";
+  cout << Add(TestStr) << endl;
+  cout << "Test0 ";
+  cout << Add(Test0) << endl;
   return 0;
 }
