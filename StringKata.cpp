a3b4e73e335b3e6d3546c722632bddbf2fd4b352
DaveHalasz <dave.halasz@7signal.com>
2019-10-09 18:57:54 -0400

    Commit for Step 3, Step 2 already handled

--- StringKata.cpp
+++ StringKata.cpp
@@ -31,15 +31,30 @@ int Add(string numbers)
 
 int main( int argc, char *argv[] )
 {
+	// Test cases for the first two steps
 	string TestStr = "1,2";
 	string NullStr = "";
 	string Test0 = "123,321,111";
 
-  cout << "NullStr ";
-  cout << Add(NullStr) << endl;
-  cout << "TestStr ";
-  cout << Add(TestStr) << endl;
-  cout << "Test0 ";
-  cout << Add(Test0) << endl;
-  return 0;
+	// Test cases for step 3
+	string Test3A = "1\n2,3";
+	// Note : Step 3 indicates the following is invalid input
+	// Assuming ok to handle, even though not valid input
+	string Test3B = "1,\n2";
+
+	// Run the first two steps unit tests
+	cout << "NullStr ";
+	cout << Add(NullStr) << endl;
+	cout << "TestStr ";
+	cout << Add(TestStr) << endl;
+	cout << "Test0 ";
+	cout << Add(Test0) << endl;
+	
+	// Run the step 3 unit tests
+	cout << "Test3A ";
+	cout << Add(Test3A) << endl;
+	cout << "Test3B ";
+	cout << Add(Test3B) << endl;
+	
+	return 0;
 }
