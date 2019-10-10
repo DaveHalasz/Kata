9d18e4032b81e01a397c1a7dd27f9f0f266183a6
DaveHalasz <dave.halasz@7signal.com>
2019-10-09 23:11:41 -0400

    Step 4 commit, allows defining the delimeter

--- StringKata.cpp
+++ StringKata.cpp
@@ -1,33 +1,68 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <string.h>
 using namespace std ;
 
 
 int Add(string numbers);
 
-// Add the numbers
+char GetDelimeter(string numbers);
+
+// Add the numbers which are contained in the string
 int Add(string numbers)
 {
 	int Total = 0;
 	
 	stringstream ss;
+	stringstream ss2;
 	ss << numbers;
 	
+	// Get the delimeter from the beginning of the string
+	char delim = GetDelimeter(numbers);
+	
 	// Continue until done with the input string
-	// Using a comma as the delimeter
-	string temp;
+	// First get a line, second pars the line with the delimeter
+	// Getting a line first fixes an issue with Test3A
+	string temp, templine;
 	int found;
-	while (getline(ss, temp, ','))
-	{	
-		// Check for an integer
-		if (stringstream(temp) >> found )
-			Total += found;
+	while (getline(ss, templine))
+	{
+		// Now parse through the line
+		ss2 << templine;
+	
+		// Parse the line with the delimeter
+		while (getline(ss2, temp, delim))
+		{
+			// Check for an integer
+			if (stringstream(temp) >> found )
+				Total += found;
+		}
+		ss2.clear();
 	}
 	
 	return Total;
 }
 
+// Get the delimeter
+// If there is no delimeter defined then default is comma
+// Delimeter starts with // and end with new line
+char GetDelimeter(string numbers)
+{
+	string delimStart = "//";
+	char delim = ',';
+
+	// Check for the delimeter markings at the begiining of the string
+	if ( !strncmp(numbers.c_str(), delimStart.c_str(), delimStart.size()) )
+	{
+		// Check that the string isn't limited to the delimStart
+		// If there is another character then it is the delimeter
+		if ( numbers.size() > delimStart.size() )
+			delim = numbers.at(delimStart.size());
+		
+	}
+	return delim;
+}
 
 int main( int argc, char *argv[] )
 {
@@ -41,6 +76,9 @@ int main( int argc, char *argv[] )
 	// Note : Step 3 indicates the following is invalid input
 	// Assuming ok to handle, even though not valid input
 	string Test3B = "1,\n2";
+	
+	// Test cases for step 4
+	string Test4A = "//;\n1;7";
 
 	// Run the first two steps unit tests
 	cout << "NullStr ";
@@ -55,6 +93,10 @@ int main( int argc, char *argv[] )
 	cout << Add(Test3A) << endl;
 	cout << "Test3B ";
 	cout << Add(Test3B) << endl;
+	
+	// Run the step 4 unit tests
+	cout << "Test4A ";
+	cout << Add(Test4A) << endl;
 	
 	return 0;
 }
